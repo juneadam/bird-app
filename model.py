ef5233bec053fb51f1737574e9e76c2cf4c652e9
bethany <bethanylynnpro@gmail.com>
2023-03-17 10:08:34 -0700

    commit

--- model.py
+++ model.py
@@ -31,15 +31,11 @@ class Bird(db.Model):
         """Create a bird using CSV data."""
 
         for line in open(cls._data_file):
-            data_name, data_state, data_latin,\ 
-            data_bird_type1, data_bird_type2, data_ability,\
-            data_description, data_image, data_call,\ 
-            data_length, data_weight, data_wingspan = line.strip().split(',')
+            data_name, data_state, data_latin, data_bird_type1, data_bird_type2, data_ability,\
+            data_description, data_image, data_call, data_length, data_weight, data_wingspan = line.strip().split(',')
             
-            return cls(data_name, data_state, data_latin,\ 
-            data_bird_type1, data_bird_type2, data_ability,\
-            data_description, data_image, data_call,\ 
-            data_length, data_weight, data_wingspan)
+            return cls(data_name, data_state, data_latin, data_bird_type1, data_bird_type2, data_ability,\
+            data_description, data_image, data_call, data_length, data_weight, data_wingspan)
 
 
     def __repr__(self):
