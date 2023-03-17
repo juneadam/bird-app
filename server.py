7e6c20a598b2ef09d7d73975dbbbb29d63b3ba90
bethany <bethanylynnpro@gmail.com>
2023-03-17 11:36:48 -0700

    added all articuno facts, no sound yet - bb

--- server.py
+++ server.py
@@ -11,6 +11,11 @@ app = Flask(__name__)
 app.secret_key = 'dev'
 app.jinja_env.undefined = StrictUndefined
 
+@app.route('/')
+def homepage():
+    """birddex homepage"""
+    return render_template('bird_homepage.html')
+
 if __name__ == "__main__":
     connect_to_db(app)
     app.run(host="0.0.0.0", debug=True)
