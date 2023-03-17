ae0d2f9235f5bac2741c38cf3d7c1f282291aade
bethany <bethanylynnpro@gmail.com>
2023-03-17 10:35:18 -0700

    very basic HTML layout - Bethany branch

--- server.py
+++ server.py
@@ -11,6 +11,12 @@ app = Flask(__name__)
 app.secret_key = 'dev'
 app.jinja_env.undefined = StrictUndefined
 
+@app.route('/')
+def homepage():
+    """view homepage"""
+
+    return render_template('bird_homepage.html')
+
 if __name__ == "__main__":
     connect_to_db(app)
     app.run(host="0.0.0.0", debug=True)
