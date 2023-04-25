79c2a82f160d8800374a6f6160bfc81706043b24
bethany <bethanylynnpro@gmail.com>
2023-04-25 13:41:35 -0700

    added folder on pull branch

---
+++ python/server.py
@@ -0,0 +1,21 @@
+# Flask routing for bird-dex app
+
+from flask import Flask, render_template, request, flash, session, redirect, jsonify
+
+from jinja2 import StrictUndefined
+
+from model import Bird, connect_to_db, db
+
+app = Flask(__name__)
+app.secret_key = 'dev'
+app.jinja_env.undefined = StrictUndefined
+
+@app.route('/')
+def homepage():
+    """view homepage"""
+
+    return render_template('index.html')
+
+if __name__ == "__main__":
+    connect_to_db(app)
+    app.run(host="0.0.0.0", debug=True)
