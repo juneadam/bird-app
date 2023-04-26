c0ab86f18b6e6ee33d0a79586a6dff95f11d4871
bethany <bethanylynnpro@gmail.com>
2023-04-26 10:40:35 -0700

    cleaned up dpad and added styling

--- src/bird-display.js
+++ src/bird-display.js
@@ -15,66 +15,58 @@ function BirdDisplay({ birds }) {
     setCurrentBirdIndex((currentBirdIndex - 1 + birds.length) % birds.length);
   };
 
-  // const handleArrowClick = (event) => {
-  //   const { offsetX, target } = event.nativeEvent;
-  //   const { width } = target.getBoundingClientRect();
-  //   if (offsetX <= width / 2) {
-  //     handlePreviousBird();
-  //   } else {
-  //     handleNextBird();
-  //   }
-  // };
-  
   return (
     <React.Fragment>
       <div id="bird-display-container">
-      {currentBird ? (
-        <React.Fragment>
-          <div id="current-bird">
-            <div id="overflow-wrapper">
-              <ImageWrapper
-                name={currentBird.name}
-                image={currentBird.image}
-                // call={currentBird.call}
-                bird_type1={currentBird.bird_type1}
-                bird_type2={currentBird.bird_type2}
-              />
-              <DescriptionWrapper
-                description={currentBird.description}
-                state={currentBird.state}
-                ability_name={currentBird.ability_name}
-                ability_text={currentBird.ability_text}
-                weight={currentBird.weight}
-                height={currentBird.height}
-                wingspan={currentBird.wingspan}
-              />
-              <div id="description-wrapper">
-                <p>{currentBird.description}</p>
-                <p>{currentBird.state}</p>
-                <p>{currentBird.ability}</p>
-                <p>{currentBird.weight}</p>
-                <p>{currentBird.length_}</p>
-                <p>{currentBird.wingspan}</p>
+        {currentBird ? (
+          <React.Fragment>
+            <div id="current-bird">
+              <div id="overflow-wrapper">
+                <ImageWrapper
+                  name={currentBird.name}
+                  image={currentBird.image}
+                  // call={currentBird.call}
+                  bird_type1={currentBird.bird_type1}
+                  bird_type2={currentBird.bird_type2}
+                />
+                <DescriptionWrapper
+                  description={currentBird.description}
+                  state={currentBird.state}
+                  ability_name={currentBird.ability_name}
+                  ability_text={currentBird.ability_text}
+                  weight={currentBird.weight}
+                  height={currentBird.height}
+                  wingspan={currentBird.wingspan}
+                />
               </div>
             </div>
-          </div>
-          <div>
-          <div className="button-container">
-              <button className="arrow-button previous-button" onClick={handlePreviousBird}>
-                <img src="arrow.png" alt="previous" />
-              </button>
-              <button className="arrow-button next-button" onClick={handleNextBird}>
-                <img src="arrow.png" alt="next" />
-              </button>
+            <div>
+              <div className="button-container">
+                <button className="arrow-button previous-button" onClick={handlePreviousBird}>
+                </button>
+                <button className="arrow-button next-button" onClick={handleNextBird}>
+                </button>
+              </div>
             </div>
-          </div>
-        </React.Fragment>
-      ) : (
-        <p className="loading-message">more birds to come</p>
-      )}
+          </React.Fragment>
+        ) : (
+          <p className="loading-message">more birds to come</p>
+        )}
       </div>
     </React.Fragment>
   );
 }
 
 export default BirdDisplay;
+
+
+
+
+{/* <div id="description-wrapper">
+<p>{currentBird.description}</p>
+<p>{currentBird.state}</p>
+<p>{currentBird.ability}</p>
+<p>{currentBird.weight}</p>
+<p>{currentBird.length_}</p>
+<p>{currentBird.wingspan}</p>
+</div> */}
