8a55e9408da1e4b6eb0007a046e7f1734085ca52
bethany <bethanylynnpro@gmail.com>
2023-04-27 10:20:15 -0700

    updated all components in description

---
+++ src/descriptioncomponents/deets-card.js
@@ -0,0 +1,18 @@
+// React module for reusable card for height, weight, and wingspan data points from JSON data
+
+import './deets-card.css';
+
+const DeetsCard = (props) => {
+    return(
+        <div className="deets-card" id={"deets-card" + props.header}>
+            <div className='deets-header'>
+                <b>{props.header}</b>
+            </div>
+            <div className='deets-body'>
+                <i>{props.data}</i>
+            </div>
+        </div>
+    )
+}
+
+export default DeetsCard;
---
+++ src/descriptioncomponents/state.js
@@ -0,0 +1,11 @@
+// React module for the state/region component 
+
+const State = (props) => {
+    return (
+        <div id="state">
+            {props.state}
+        </div>
+    )
+}
+
+export default State;
