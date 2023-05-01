5f7031ae5760e3af69827577b2226d62874abeca
bethany <bethanylynnpro@gmail.com>
2023-05-01 16:37:39 -0700

    added some styling

--- src/App.js
+++ src/App.js
@@ -20,13 +20,13 @@ function App() {
     };
 
     fetchBirds();
-  }, []);
+  }, [birds]);
 
   console.log('rendering app with birds: ', birds); 
   
   return (
     <div>
-      <h1>bee-bop-bird-dex</h1>
+      <h1>   </h1>
       {birds.length > 0 ? (
         <BirdDisplay birds={birds} />
       ) : (
