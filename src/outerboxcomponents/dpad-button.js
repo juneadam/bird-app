782e774c3f1662e4a480fcf1fcfcca0e5bed73cb
bethany <bethanylynnpro@gmail.com>
2023-05-01 16:38:00 -0700

    added css for button

---
+++ src/outerboxcomponents/dpad-button.js
@@ -0,0 +1,15 @@
+// import './dpad-button.css';
+
+// const dpadButton = (props) => {
+
+//     return (
+//         <div className="button-container">
+//             <button className="arrow-button previous-button" onClick={handlePreviousBird}>
+//             </button>
+//             <button className="arrow-button next-button" onClick={handleNextBird}>
+//             </button>
+//         </div>
+//     )
+// }
+
+// export default dpadButton
