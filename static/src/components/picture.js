877a22014774ff863493e9d9b4458ec7f1f4eba4
bethany <bethanylynnpro@gmail.com>
2023-05-01 15:47:31 -0700

    deleted old templates

--- static/src/components/picture.js
+++
@@ -1,9 +0,0 @@
-// const BirdPhoto = (props) => {
-//     return (
-//         <div id="bird-pic">
-//             <img src="/static/img/Articuno.png" style="width: 75%; height: auto;" />
-//         </div>
-//     )
-// }
-
-// export default BirdPhoto;
